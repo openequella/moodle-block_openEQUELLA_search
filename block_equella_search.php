be9e77322cdd61fa6ee362aad34758f7765c111c
Dongsheng Cai <imdongsheng@gmail.com>
2013-11-27 14:08:42 +1100

    Unix file format

--- block_equella_search.php
+++ block_equella_search.php
@@ -17,13 +17,12 @@
 
 class block_equella_search extends block_list {
 
-	function init() {
-		$this->title = get_string('title', 'block_equella_search');
-	}
+    function init() {
+        $this->title = get_string('title', 'block_equella_search');
+    }
 
-	function get_content() {
-
-		global $CFG, $USER, $SITE, $COURSE;
+    function get_content() {
+        global $CFG, $USER, $SITE, $COURSE, $DB;
 
         if( $this->content !== NULL ) {
             return $this->content;
@@ -32,72 +31,69 @@ class block_equella_search extends block_list {
             return null;
         }
 
+        $this->content = new stdClass;
+        $this->content->items = array();
+        $this->content->icons = array();
+        if(isset($this->config->search_type1)) {
+            $this->content->search_type1 = $this->config->search_type1;
+        }
+        if(isset($this->config->search_type2)) {
+            $this->content->search_type2 = $this->config->search_type2;
+        }
+        if(isset($this->config->collection1)) {
+            $this->content->collection1 = $this->config->collection1;
+        }
+        if(isset($this->config->collection2)) {
+            $this->content->collection2 = $this->config->collection2;
+        }
+        if(isset($this->config->collection3)) {
+            $this->content->collection3 = $this->config->collection3;
+        }
+        $this->content->footer = '';
 
-		$this->content = new stdClass;
-		$this->content->items = array();
-		$this->content->icons = array();
-		if(isset($this->config->search_type1)){
-			$this->content->search_type1 = $this->config->search_type1;
-		}
-		if(isset($this->config->search_type2)){
-			$this->content->search_type2 = $this->config->search_type2;
-		}
-		if(isset($this->config->collection1)){
-			$this->content->collection1 = $this->config->collection1;
-		}
-		if(isset($this->config->collection2)){
-			$this->content->collection2 = $this->config->collection2;
-		}
-		if(isset($this->config->collection3)){
-			$this->content->collection3 = $this->config->collection3;
-		}
-		$this->content->footer = '';
+        if (empty($this->instance->pageid)) { // sticky
+            if (!empty($COURSE)) {
+                $this->instance->pageid = $COURSE->id;
+            }
+        }
 
-		if (empty($this->instance->pageid)) { // sticky
-			if (!empty($COURSE)) {
-				$this->instance->pageid = $COURSE->id;
-			}
-		}
+        if (!empty($this->instance->pageid)) {
+            $context = get_context_instance(CONTEXT_COURSE, $this->instance->pageid);
+            if ($COURSE->id == $this->instance->pageid) {
+                $course = $COURSE;
+            } else {
+                $course = $DB->get_record('course', array('id'=>$this->instance->pageid));
+            }
+        } else {
+            $context = get_context_instance(CONTEXT_SYSTEM);
+            $course = $SITE;
+        }
 
-		if (!empty($this->instance->pageid)) {
-			$context = get_context_instance(CONTEXT_COURSE, $this->instance->pageid);
-			if ($COURSE->id == $this->instance->pageid) {
-				$course = $COURSE;
-			} else {
-				$course = get_record('course', 'id', $this->instance->pageid);
-			}
-		} else {
-			$context = get_context_instance(CONTEXT_SYSTEM);
-			$course = $SITE;
-		}
+        if (!has_capability('block/equella_search:view', $context)) {  // Just return
+            return $this->content;
+        }
 
-		if (!has_capability('block/equella_search:view', $context)) {  // Just return
-			return $this->content;
-		}
+        /// Search Equella
+        if ($course->id !== SITEID and has_capability('block/equella_search:search', $context)) {
+            $equellasearchurl = new moodle_url('/blocks/equella_search/equella_search_api.php', array('courseid'=>$this->instance->pageid));
+            $this->content->items[] = html_writer::link($equellasearchurl, get_string('searchaction', 'block_equella_search'));
+            $iconurl = new moodle_url('/mod/equella/pix/icon-red.gif');
+            $icon = html_writer::empty_tag('img', array('src' => $iconurl, 'class' => 'icon', 'alt' => 'equella-icon'));
+            $this->content->icons[]= $icon;
+        }
 
-		/// Search Equella
-		if ($course->id !== SITEID and has_capability('block/equella_search:search', $context)) {
-                        $equellasearchurl = new moodle_url('/blocks/equella_search/equella_search_api.php', array('courseid'=>$this->instance->pageid));
-                        $this->content->items[] = html_writer::link($equellasearchurl, get_string('searchaction', 'block_equella_search'));
-                        $iconurl = new moodle_url('/mod/equella/pix/icon-red.gif');
-                        $icon = html_writer::empty_tag('img', array('src' => $iconurl, 'class' => 'icon', 'alt' => 'equella-icon'));
-			$this->content->icons[]= $icon;
-		}
+        return $this->content;
+    }
 
-		return $this->content;
-	}
+    function applicable_formats() {
+        return array('course' => true);   // Not needed on site
+    }
 
-	function applicable_formats() {
-		return array('course' => true);   // Not needed on site
-	}
+    function has_config() {
+        return false;
+    }
 
-	function has_config() {
-		return false;
-	}
-
-	function instance_allow_config() {
-		return true;
-	}
+    function instance_allow_config() {
+        return true;
+    }
 }
-
-?>
